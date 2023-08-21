a2def6138369cc8eab5fcc09ae2bd1c9c2b055be
SeHee Son <sehee@ixlayer.com>
2023-08-21 15:44:48 -0700

    feat(app): chart app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Users Gained",
+        data: [],
+      },
+    ],
+  });
+  const [stockSymbolInput, setStockSymbolInput] = useState('');
+  const [dayInput, setDayInput] = useState('');
+  const apiKey = 'BAX3XPJAS4JPJ8XO'; 
+
+  const fetchData = async (stockSymbol, days = 10) => {
+    try {
+      const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSymbol}&outputsize=compact&apikey=${apiKey}`);
+
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      
+      const parsedData = await response.json();
+      const dailyData = parsedData["Time Series (Daily)"]
+
+      const labels = [];
+      const userGainData = [];
+      let entryCount = 0
+
+      for (const date in dailyData) {
+        if (entryCount >= days) { // Stop processing after 30 entries
+          break;
+        }
+        const entry = dailyData[date];
+        labels.push(date);
+        userGainData.push(parseFloat(entry["1. open"]));
+        entryCount++;
+      }
+
+      setUserData({
+        labels,
+        datasets: [
+          {
+            label: "Users Gained",
+            data: userGainData,
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0",
+            ],
+            borderColor: "black",
+            borderWidth: 2,
+          },
+        ],
+      });
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleFetchButtonClick = () => {
+    fetchData(stockSymbolInput, dayInput);
+  };
+
+  useEffect(() => {
+  }, [userData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container pt-md-4">
+      <div className="row pt-md-5">
+        <div className="form-group col-md-8">
+          <label for="stockSymbol">Stock symbol</label>
+          <input
+            id="stockSymbol"
+            type="text"
+            className="form-control" 
+            value={stockSymbolInput}
+            onChange={(e) => setStockSymbolInput(e.target.value)}
+            placeholder="Enter stock symbol" 
+          />
+        </div>
+        <div className="form-group col-md-2">
+          <label for="days">Day(s)</label>
+          <input
+            id="days"
+            value={dayInput}
+            onChange={(e) => setDayInput(e.target.value)}
+            className="form-control" 
+            placeholder="Enter days"
+          />
+        </div>
+        <button
+          className="btn btn-primary col-md-2"
+          onClick={handleFetchButtonClick}
         >
-          Learn React
-        </a>
-      </header>
+          Get Data
+        </button>
+      </div>
+
+
+        <div className="row">
+          <div className="col-md-4">
+            <BarChart chartData={userData} />
+          </div>
+          <div className="col-md-4">
+            <LineChart chartData={userData} />
+          </div>
+          <div className="col-md-4">
+            <PieChart chartData={userData} />
+          </div>
+        </div>
+
     </div>
   );
 }
